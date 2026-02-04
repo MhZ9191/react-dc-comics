349eb6958306a0d3a4b7311e7d22305a55bcb669
MhZ9191 <hegic91@gmail.com>
2026-02-04 15:46:26 +0100

    Added components

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./assets/css/index.css";
 import App from "./App.jsx";
+import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
+import Footer from "./components/Footer.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Header />
+    <Main />
+    <Footer />
   </StrictMode>,
 );
