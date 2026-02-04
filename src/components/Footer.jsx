09c3c27c2740e5a8e209063ab9a98e88ef0f7bea
MhZ9191 <hegic91@gmail.com>
2026-02-04 18:23:38 +0100

    Added first section footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,52 @@
 export default function Footer() {
   return (
     <>
-      <footer>footer</footer>
+      <footer>
+        <section className="first-sec-footer">
+          <div className="first-div-footer">
+            <div className="first-div-mini-card">
+              <div>
+                <figure>
+                  <img src="/buy-comics-digital-comics.png" alt="digital" />
+                </figure>
+              </div>
+              <span>DIGITAL COMICS</span>
+            </div>
+            <div className="first-div-mini-card">
+              <div>
+                <figure>
+                  <img src="/buy-comics-shop-locator.png" alt="shop" />
+                </figure>
+              </div>
+              <span>COMIC SHOP LOCATOR</span>
+            </div>
+            <div className="first-div-mini-card">
+              <div>
+                <figure>
+                  <img src="/buy-comics-merchandise.png" alt="merc" />
+                </figure>
+              </div>
+              <span>DC MERCHANDISE</span>
+            </div>
+            <div className="first-div-mini-card">
+              <div>
+                <figure>
+                  <img src="/buy-comics-subscriptions.png" alt="sub" />
+                </figure>
+              </div>
+              <span>SUBSCRIPTION</span>
+            </div>
+            <div className="first-div-mini-card">
+              <div>
+                <figure>
+                  <img src="/buy-dc-power-visa.svg" alt="visa" />
+                </figure>
+              </div>
+              <span>DC POWER VISA</span>
+            </div>
+          </div>
+        </section>
+      </footer>
     </>
   );
 }
