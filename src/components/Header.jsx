1714ca858d0499612b537d13b7f33abc45bffaa3
MhZ9191 <hegic91@gmail.com>
2026-02-04 17:07:54 +0100

    Added header responsive but not event listener on click

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,84 @@
 export default function Header() {
   return (
     <>
-      <header>header</header>
+      <header>
+        <section className="sec-header">
+          <div className="div-header">
+            <div className="logo-header">
+              <img src="/dc-logo.png" alt="dc-logo" />
+            </div>
+            <div>
+              <div className="btn-menu"></div>
+              <div className="menu">
+                <ul className="menu-link hidden">
+                  <li>
+                    <a href="#">test</a>
+                  </li>
+                  <li>
+                    <a href="#">test</a>
+                  </li>
+                  <li>
+                    <a href="#">test</a>
+                  </li>
+                  <li>
+                    <a href="#">test</a>
+                  </li>
+                  <li>
+                    <a href="#">test</a>
+                  </li>
+                  <li>
+                    <a href="#">test</a>
+                  </li>
+                  <li>
+                    <a href="#">test</a>
+                  </li>
+                  <li>
+                    <a href="#">test</a>
+                  </li>
+                  <li>
+                    <a href="#">test</a>
+                  </li>
+                  <li>
+                    <a href="#">test</a>
+                  </li>
+                </ul>
+              </div>
+              <ul className="link-header">
+                <li>
+                  <a href="#">testtest</a>
+                </li>
+                <li>
+                  <a href="#">test</a>
+                </li>
+                <li>
+                  <a href="#">test</a>
+                </li>
+                <li>
+                  <a href="#">test</a>
+                </li>
+                <li>
+                  <a href="#">test</a>
+                </li>
+                <li>
+                  <a href="#">test</a>
+                </li>
+                <li>
+                  <a href="#">test</a>
+                </li>
+                <li>
+                  <a href="#">test</a>
+                </li>
+                <li>
+                  <a href="#">test</a>
+                </li>
+                <li>
+                  <a href="#">test</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </header>
     </>
   );
 }
