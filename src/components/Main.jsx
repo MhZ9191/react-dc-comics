43aefac5f8ce288dfe00fcb88cb6291658b98c37
MhZ9191 <hegic91@gmail.com>
2026-02-05 14:46:30 +0100

    Added card map()

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,28 @@
+import Comics from "./Comics";
+
 export default function Main() {
   return (
     <>
       <main>
         <section className="sec-main">
           <div className="div-main">
-            <span>--&gt; Content goes here &lt;--</span>
+            {Comics.map(({ id, title, thumb }) => {
+              return (
+                <div key={id} className="card">
+                  <figure>
+                    <img src={thumb} alt={title} />
+                    <figcaption>{title}</figcaption>
+                  </figure>
+                </div>
+              );
+            })}
+
+            {/* <div className="card">
+              <figure>
+                <img src={Comics[0].thumb} alt={Comics[0].title} />
+                <figcaption>{Comics[0].title}</figcaption>
+              </figure>
+            </div> */}
           </div>
         </section>
       </main>
