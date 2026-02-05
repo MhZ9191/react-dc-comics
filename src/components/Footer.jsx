bf30d2940f7c9e2799b4d23d82d6e45fe9f44f1a
MhZ9191 <hegic91@gmail.com>
2026-02-05 16:19:04 +0100

    Added first section footer map()

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,49 +1,44 @@
+const miniCard = [
+  {
+    source: "/buy-comics-digital-comics.png",
+    title: "DIGITAL COMICS",
+  },
+  {
+    source: "/buy-comics-shop-locator.png",
+    title: "COMIC SHOP LOCATOR",
+  },
+  {
+    source: "/buy-comics-merchandise.png",
+    title: "DC MERCHANDISE",
+  },
+  {
+    source: "/buy-comics-subscriptions.png",
+    title: "SUBSCRIPTION",
+  },
+  {
+    source: "/buy-dc-power-visa.svg",
+    title: "DC POWER VISA",
+  },
+];
+
 export default function Footer() {
   return (
     <>
       <footer>
         <section className="first-sec-footer">
           <div className="first-div-footer">
-            <div className="first-div-mini-card">
-              <div>
-                <figure>
-                  <img src="/buy-comics-digital-comics.png" alt="digital" />
-                </figure>
-              </div>
-              <span>DIGITAL COMICS</span>
-            </div>
-            <div className="first-div-mini-card">
-              <div>
-                <figure>
-                  <img src="/buy-comics-shop-locator.png" alt="shop" />
-                </figure>
-              </div>
-              <span>COMIC SHOP LOCATOR</span>
-            </div>
-            <div className="first-div-mini-card">
-              <div>
-                <figure>
-                  <img src="/buy-comics-merchandise.png" alt="merc" />
-                </figure>
-              </div>
-              <span>DC MERCHANDISE</span>
-            </div>
-            <div className="first-div-mini-card">
-              <div>
-                <figure>
-                  <img src="/buy-comics-subscriptions.png" alt="sub" />
-                </figure>
-              </div>
-              <span>SUBSCRIPTION</span>
-            </div>
-            <div className="first-div-mini-card">
-              <div>
-                <figure>
-                  <img src="/buy-dc-power-visa.svg" alt="visa" />
-                </figure>
-              </div>
-              <span>DC POWER VISA</span>
-            </div>
+            {miniCard.map(({ source, title }) => {
+              return (
+                <div className="first-div-mini-card">
+                  <div>
+                    <figure>
+                      <img src={source} alt={title} />
+                    </figure>
+                  </div>
+                  <span>{title}</span>
+                </div>
+              );
+            })}
           </div>
         </section>
 
